cad37e72963fb17b365984f56d67e1d98626c7ba
Justin Hines <justinhines@bit.ly>
2013-02-20 22:18:47 +0000

    go fmt

--- bitly_api.go
+++ bitly_api.go
@@ -1,149 +1,148 @@
 package bitly_api
 
 import (
-        "net/url"
-        "fmt"
-        "strings"
-        "net/http"
-        "io/ioutil"
-        "github.com/bitly/go-simplejson"
+	"fmt"
+	"github.com/bitly/go-simplejson"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 type Json struct {
-  data interface{}
+	data interface{}
 }
 
 type Connection struct {
-  login string
-  apiKey string
-  accessToken string
-  secret string
+	login       string
+	apiKey      string
+	accessToken string
+	secret      string
 }
 
-func NewConnection (accessToken string, secret string) *Connection{
-  c := new(Connection)
-  c.accessToken = accessToken
-  c.secret = secret
-  return c
+func NewConnection(accessToken string, secret string) *Connection {
+	c := new(Connection)
+	c.accessToken = accessToken
+	c.secret = secret
+	return c
 }
 
 func NewConnectionApiKey(apiKey string, login string, secret string) *Connection {
-  c := new(Connection)
-  c.apiKey = apiKey
-  c.login = login
-  c.secret = secret
-  return c
+	c := new(Connection)
+	c.apiKey = apiKey
+	c.login = login
+	c.secret = secret
+	return c
 }
 
-func (c *Connection) ShortenWithDomain (uri string,  preferredDomain string) (map[string]interface{}, error) {
-    params := url.Values{}
-    params.Set("uri", uri)
-    params.Set("domain", preferredDomain)
-    return c.shorten(params)
+func (c *Connection) ShortenWithDomain(uri string, preferredDomain string) (map[string]interface{}, error) {
+	params := url.Values{}
+	params.Set("uri", uri)
+	params.Set("domain", preferredDomain)
+	return c.shorten(params)
 }
 
 func (c *Connection) Shorten(uri string) (map[string]interface{}, error) {
-    params := url.Values{}
-    params.Set("uri", uri)
-    return c.shorten(params)
+	params := url.Values{}
+	params.Set("uri", uri)
+	return c.shorten(params)
 }
 
-func hashOrUrl (mystery string) string {
-    var solved string
-    if strings.Contains(mystery, "/") {
-        solved = "shortUrl"
-    } else {
-        solved = "hash"
-    }
-    return solved
+func hashOrUrl(mystery string) string {
+	var solved string
+	if strings.Contains(mystery, "/") {
+		solved = "shortUrl"
+	} else {
+		solved = "hash"
+	}
+	return solved
 }
 
-func (c *Connection) shorten(params url.Values) (map[string]interface{}, error){
-    return c.call("shorten", params, false)
+func (c *Connection) shorten(params url.Values) (map[string]interface{}, error) {
+	return c.call("shorten", params, false)
 }
 
 func (c *Connection) Expand(arg string) (map[string]interface{}, error) {
-  params := url.Values{}
-  atype := hashOrUrl(arg)
-  params.Set(atype, arg)
-  return c.call("expand", params, true)
+	params := url.Values{}
+	atype := hashOrUrl(arg)
+	params.Set(atype, arg)
+	return c.call("expand", params, true)
 }
 
-func (c *Connection) Clicks (arg string) (map[string]interface{}, error) {
-  params := url.Values{}
-  atype := hashOrUrl(arg)
-  params.Set(atype, arg)
-  return c.call("clicks", params, true)
+func (c *Connection) Clicks(arg string) (map[string]interface{}, error) {
+	params := url.Values{}
+	atype := hashOrUrl(arg)
+	params.Set(atype, arg)
+	return c.call("clicks", params, true)
 }
 
-func (c *Connection) ClicksByDay (arg string) (map[string]interface{}, error) {
-  params := url.Values{}
-  atype := hashOrUrl(arg)
-  params.Set(atype, arg)
-  return c.call("clicks_by_day", params, true)
+func (c *Connection) ClicksByDay(arg string) (map[string]interface{}, error) {
+	params := url.Values{}
+	atype := hashOrUrl(arg)
+	params.Set(atype, arg)
+	return c.call("clicks_by_day", params, true)
 }
 
 func (c *Connection) ClicksByMinute(arg string) (map[string]interface{}, error) {
-  params := url.Values{}
-  atype := hashOrUrl(arg)
-  params.Set(atype, arg)
-  return c.call("clicks_by_minute", params, true)
+	params := url.Values{}
+	atype := hashOrUrl(arg)
+	params.Set(atype, arg)
+	return c.call("clicks_by_minute", params, true)
 }
 
-func (c *Connection) call (endpoint string, params url.Values, array_wrapped bool) (map[string]interface{}, error) {
+func (c *Connection) call(endpoint string, params url.Values, array_wrapped bool) (map[string]interface{}, error) {
 
-    var scheme string
-    var host string
-    
-    if c.accessToken != "" {
-        scheme = "https"
-        params.Set("access_token", c.accessToken)
-        host = "api-ssl.bit.ly"
-    } else {
-        scheme = "http"
-        params.Set("login", c.login)
-        params.Set("apiKey", c.apiKey)
-        host = "api.bit.ly"
-    }
+	var scheme string
+	var host string
 
-    // if c.secret != "" {
-        // params.Set("signature", generateSignature(params, c.secret))
-    // }
-    request_url := fmt.Sprintf("%s://%s/v3/%s?%s", scheme, host, endpoint, params.Encode())
+	if c.accessToken != "" {
+		scheme = "https"
+		params.Set("access_token", c.accessToken)
+		host = "api-ssl.bit.ly"
+	} else {
+		scheme = "http"
+		params.Set("login", c.login)
+		params.Set("apiKey", c.apiKey)
+		host = "api.bit.ly"
+	}
 
-    
-    http_client := &http.Client{}
-    request, err := http.NewRequest("GET", request_url, nil)
-    if err != nil {
-        return nil, err
-    }
+	// if c.secret != "" {
+	// params.Set("signature", generateSignature(params, c.secret))
+	// }
+	request_url := fmt.Sprintf("%s://%s/v3/%s?%s", scheme, host, endpoint, params.Encode())
 
-    request.Header.Add("User-agent", "Go/bitly-api")
-    response, err := http_client.Do(request)
-    if err != nil {
-        return nil, err
-    }
+	http_client := &http.Client{}
+	request, err := http.NewRequest("GET", request_url, nil)
+	if err != nil {
+		return nil, err
+	}
 
-    defer response.Body.Close()
-    contents, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        return nil, err
-    }
+	request.Header.Add("User-agent", "Go/bitly-api")
+	response, err := http_client.Do(request)
+	if err != nil {
+		return nil, err
+	}
 
-    js, err := simplejson.NewJson([]byte(contents))
-    if err != nil {
-        return nil, err
-    }
-    
-    var data map[string]interface{}
-    if array_wrapped {
-        data, err = js.Get("data").Get(endpoint).GetIndex(0).Map()
-    } else {
-        data, err = js.Get("data").Map()
-    }
-    if err != nil {
-        return nil, err
-    }
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
 
-    return data, nil
+	js, err := simplejson.NewJson([]byte(contents))
+	if err != nil {
+		return nil, err
+	}
+
+	var data map[string]interface{}
+	if array_wrapped {
+		data, err = js.Get("data").Get(endpoint).GetIndex(0).Map()
+	} else {
+		data, err = js.Get("data").Map()
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
--- bitly_api_test.go
+++ bitly_api_test.go
@@ -1,110 +1,109 @@
 package bitly_api
 
 import (
-        "os"
-        "testing"
+	"os"
+	"testing"
 )
-
 
 func getConnection(t *testing.T) *Connection {
 
-    token := "BITLY_ACCESS_TOKEN"
+	token := "BITLY_ACCESS_TOKEN"
 
-    BITLY_ACCESS_TOKEN := os.Getenv(token)
-    if BITLY_ACCESS_TOKEN == "" {
-        t.Fatalf(token + " not found")
-        return nil
-    }
-    accessToken := BITLY_ACCESS_TOKEN
-    return NewConnection(accessToken, "")
+	BITLY_ACCESS_TOKEN := os.Getenv(token)
+	if BITLY_ACCESS_TOKEN == "" {
+		t.Fatalf(token + " not found")
+		return nil
+	}
+	accessToken := BITLY_ACCESS_TOKEN
+	return NewConnection(accessToken, "")
 }
 
 func TestApi(t *testing.T) {
-    bitly := getConnection(t)
-    if bitly == nil {
-        t.Fatalf("bitly connection returned nil")
-    }
-    testUrl := "http://google.com/"
-    data, err := bitly.Shorten(testUrl)
-    if err != nil {
-        t.Fatalf("bitly Shorten returned an err %s", err)
-    }
-    
-    longUrl := data["long_url"].(string)
-    if testUrl != longUrl {
-        t.Fatalf("test url != long url from return")
-    }
+	bitly := getConnection(t)
+	if bitly == nil {
+		t.Fatalf("bitly connection returned nil")
+	}
+	testUrl := "http://google.com/"
+	data, err := bitly.Shorten(testUrl)
+	if err != nil {
+		t.Fatalf("bitly Shorten returned an err %s", err)
+	}
 
-    hash := data["hash"].(string)
-    if hash == "" {
-        t.Fatalf("hash empty")
-    }
+	longUrl := data["long_url"].(string)
+	if testUrl != longUrl {
+		t.Fatalf("test url != long url from return")
+	}
+
+	hash := data["hash"].(string)
+	if hash == "" {
+		t.Fatalf("hash empty")
+	}
 }
 
-func TestExpand (t *testing.T) {
-    bitly := getConnection(t)
-    if bitly == nil {
-        t.Fatalf("bitly connection returned nil")
-    }
-    data, err := bitly.Expand("test1_random_fjslfjieljfklsjflkas")
-    if err != nil {
-        t.Fatalf("bitly Expand returned an error %s", err)
-    }
-    if data["error"] != "NOT_FOUND" {
-        t.Fatalf("bitly Expand did not return NOT_FOUND", err)
-    }
+func TestExpand(t *testing.T) {
+	bitly := getConnection(t)
+	if bitly == nil {
+		t.Fatalf("bitly connection returned nil")
+	}
+	data, err := bitly.Expand("test1_random_fjslfjieljfklsjflkas")
+	if err != nil {
+		t.Fatalf("bitly Expand returned an error %s", err)
+	}
+	if data["error"] != "NOT_FOUND" {
+		t.Fatalf("bitly Expand did not return NOT_FOUND", err)
+	}
 }
 
-func TestClicks (t *testing.T) {
-    bitly := getConnection(t)
-    if bitly == nil {
-        t.Fatalf("bitly connection returned nil")
-    }
-    data, err := bitly.Clicks("test1_random_fjslfjieljfklsjflkas")
-    if err != nil {
-        t.Fatalf("bitly clicks returned an error %s", err)
-    }
-    if data["error"] != "NOT_FOUND" {
-        t.Fatalf("bitly ClicksHash did not return NOT_FOUND", err)
-    }
-    
-    data, err = bitly.ClicksByDay("test1_random_fjslfjieljfklsjflkas")
-    if err != nil {
-        t.Fatalf("bitly clicks returned an error %s", err)
-    }
-    if data["error"] != "NOT_FOUND" {
-        t.Fatalf("bitly ClicksHash did not return NOT_FOUND", err)
-    }
-    
-    data, err = bitly.ClicksByMinute("test1_random_fjslfjieljfklsjflkas")
-    if err != nil {
-        t.Fatalf("bitly clicks returned an error %s", err)
-    }
-    if data["error"] != "NOT_FOUND" {
-        t.Fatalf("bitly ClicksHash did not return NOT_FOUND", err)
-    }
-    data, err = bitly.Clicks("http://bit.ly/3hQYj")
-    if err != nil {
-        t.Fatalf("bitly clicks returned an error %s", err)
-    }
-    if data["error"] == "NOT_FOUND" {
-        t.Fatalf("bitly ClicksHash did not return NOT_FOUND", err)
-    }
-    
-    data, err = bitly.ClicksByDay("http://bit.ly/3hQYj")
-    if err != nil {
-        t.Fatalf("bitly clicks returned an error %s", err)
-    }
-    if data["error"] == "NOT_FOUND" {
-        t.Fatalf("bitly ClicksHash did not return NOT_FOUND", err)
-    }
-    
-    data, err = bitly.ClicksByMinute("http://bit.ly/3hQYj")
-    if err != nil {
-        t.Fatalf("bitly clicks returned an error %s", err)
-    }
-    if data["error"] == "NOT_FOUND" {
-        t.Fatalf("bitly ClicksHash did not return NOT_FOUND", err)
-    }
+func TestClicks(t *testing.T) {
+	bitly := getConnection(t)
+	if bitly == nil {
+		t.Fatalf("bitly connection returned nil")
+	}
+	data, err := bitly.Clicks("test1_random_fjslfjieljfklsjflkas")
+	if err != nil {
+		t.Fatalf("bitly clicks returned an error %s", err)
+	}
+	if data["error"] != "NOT_FOUND" {
+		t.Fatalf("bitly ClicksHash did not return NOT_FOUND", err)
+	}
+
+	data, err = bitly.ClicksByDay("test1_random_fjslfjieljfklsjflkas")
+	if err != nil {
+		t.Fatalf("bitly clicks returned an error %s", err)
+	}
+	if data["error"] != "NOT_FOUND" {
+		t.Fatalf("bitly ClicksHash did not return NOT_FOUND", err)
+	}
+
+	data, err = bitly.ClicksByMinute("test1_random_fjslfjieljfklsjflkas")
+	if err != nil {
+		t.Fatalf("bitly clicks returned an error %s", err)
+	}
+	if data["error"] != "NOT_FOUND" {
+		t.Fatalf("bitly ClicksHash did not return NOT_FOUND", err)
+	}
+	data, err = bitly.Clicks("http://bit.ly/3hQYj")
+	if err != nil {
+		t.Fatalf("bitly clicks returned an error %s", err)
+	}
+	if data["error"] == "NOT_FOUND" {
+		t.Fatalf("bitly ClicksHash did not return NOT_FOUND", err)
+	}
+
+	data, err = bitly.ClicksByDay("http://bit.ly/3hQYj")
+	if err != nil {
+		t.Fatalf("bitly clicks returned an error %s", err)
+	}
+	if data["error"] == "NOT_FOUND" {
+		t.Fatalf("bitly ClicksHash did not return NOT_FOUND", err)
+	}
+
+	data, err = bitly.ClicksByMinute("http://bit.ly/3hQYj")
+	if err != nil {
+		t.Fatalf("bitly clicks returned an error %s", err)
+	}
+	if data["error"] == "NOT_FOUND" {
+		t.Fatalf("bitly ClicksHash did not return NOT_FOUND", err)
+	}
 
 }
